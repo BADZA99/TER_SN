745db077dc24f5566a365b0c6478e62e8951d4b3
papabn <pndiaye999@gmail.com>
2023-12-26 02:31:30 +0000

    Add axios and react-hook-form dependencies, update Register page UI and sign up functionality

--- FrontEnd/src/pages/Register.jsx
+++ FrontEnd/src/pages/Register.jsx
@@ -1,9 +1,114 @@
-import React from 'react'
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "90vh",
+    backgroundColor: "#f8f9fa",
+  },
+  form: {
+    display: "flex",
+    height: "60%",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    width: "350px",
+    padding: "20px",
+    borderRadius: "5px",
+    backgroundColor: "#fff",
+    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
+  },
+  input: {
+    marginBottom: "5px",
+    padding: "15px",
+    borderRadius: "5px",
+    width: "85%",
+    border: "1px solid #ddd",
+  },
+  button: {
+    padding: "10px",
+    borderRadius: "5px",
+    border: "none",
+    backgroundColor: "#303f9f",
+    color: "#fff",
+    cursor: "pointer",
+  },
+  error: {
+    color: "red",
+  },
+};
 
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      
+      const response = await axios.post("http://localhost:8000/api/register", {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      });
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+    // useNavigate("/");
+    // console.log(data);
+  };
+
   return (
-    <div>
-      register page
+    <div style={styles.container}>
+      <form style={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Nom"
+          style={styles.input}
+          {...register("name", { required: true, maxLength: 20, minLength: 5 })}
+        />
+        {errors.name && errors.name.type === "required" && (
+          <span style={styles.error}>Le champ name est requis</span>
+        )}
+        <input
+          type="email"
+          placeholder="Email"
+          style={styles.input}
+          {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        {errors.email && errors.email.type === "required" && (
+          <span style={styles.error}>Le champ email est requis</span>
+        )}
+        {errors.email && errors.email.type === "pattern" && (
+          <span style={styles.error}>Le champ email est invalide</span>
+        )}
+        <input
+          type="password"
+          placeholder="Mot de passe"
+          style={styles.input}
+          {...register("password", {
+            required: true,
+            maxLength: 20,
+            minLength: 5,
+          })}
+        />
+        {errors.password && errors.password.type === "required" && (
+          <span style={styles.error}>Le champ password est requis</span>
+        )}
+        <button type="submit" style={styles.button}>
+          Inscription
+        </button>
+      </form>
     </div>
-  )
+  );
 }
